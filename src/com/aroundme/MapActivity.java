bb5c1e186948e4f2cdfbf2834fa138eee0c2a94e
tom243 <tomer.luster@gmail.com>
2015-08-19 20:13:17 +0300

    change default picture

--- src/com/aroundme/MapActivity.java
+++ src/com/aroundme/MapActivity.java
@@ -21,7 +21,6 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.internal.cu;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
